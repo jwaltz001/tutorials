bb8da4c71043c59447faf7678ae174a87005b873
Justin Waltz <jwaltz001@gmail.com>
2019-10-21 12:20:23 -0500

    Quick Sort works with heavy help from notes

--- practice/myGA-algorithms.js
+++ practice/myGA-algorithms.js
@@ -1,6 +1,6 @@
 function mergeSort(arr, n=1) {
 	if(arr.length > 1){
-		n++
+		n++;
 		const arr1 = arr.splice(0,Math.floor(arr.length/2));
 	    console.log(arr, n)
 		console.log(arr1, n);
@@ -9,30 +9,41 @@ function mergeSort(arr, n=1) {
 	return arr
 }
 
-function quickSort(arr, startIndex=1, endIndex=arr.length-1){
-	if (startIndex != endIndex) {
-		const pivot = arr[0]
-		console.log("pivot", pivot);
-		while (startIndex < endIndex) {
-			if (arr[startIndex] >= pivot) {
-				let temp = arr[compareIndex];
-				arr[compareIndex] = arr[startIndex];
-				arr[startIndex] = temp;
-				compareIndex++;
-				startIndex++;
-			}else{
-				compareIndex++;
-			}
+function quickSort(arr, leftIndex=0, rightIndex=arr.length-1){
+	console.log("start of a quicksort call:\n", "arr", arr, "leftIndex", leftIndex, "rightIndex", rightIndex);
+	if (leftIndex < rightIndex) {
+		let pivotIndex = rightIndex;
+		//Math.floor(Math.random() * (rightIndex - leftIndex));
+		console.log("pivotIndex",pivotIndex, "pivotValue", arr[pivotIndex]);
+		const partitionIndex = partition(arr,pivotIndex,leftIndex,rightIndex);
 
-		}
-		let temp = arr[startIndex-1];
-		arr[startIndex-1] = arr[0];
-		arr[0] = temp;
-		console.log(arr,startIndex,compareIndex);
-		return quickSort(arr,);
-	}else{
-		return arr;
+		quickSort(arr, leftIndex, partitionIndex-1);
+		quickSort(arr, partitionIndex + 1, rightIndex);
 	}
+	console.log("FINAL", arr);
+	return arr;
+};
+
+//Helper functions for Quick Sort
+function partition(arr, pivotIndex, leftIndex, rightIndex){
+	const pivotValue = arr[pivotIndex];
+	let partitionIndex = leftIndex;
+
+	for (var i = leftIndex; i < rightIndex; i++) {
+		if (arr[i] < pivotValue) {
+			swap(arr, i, partitionIndex);
+			partitionIndex++;
+		}
+	}
+	swap(arr,rightIndex, partitionIndex);
+	console.log("arr at end of a partition call", arr);
+	return partitionIndex;
+}
+
+function swap(arr, indexToSwap1, indexToSwap2){
+	let tempValue = arr[indexToSwap1];
+	arr[indexToSwap1] = arr[indexToSwap2];
+	arr[indexToSwap2] = tempValue;
 }
 
 // HELPER FUNCTION: merge two sorted arrays
@@ -66,7 +77,7 @@ function merge(arr1, arr2, n) {
     const myArray = [12,6,3,7,13,8];
     const sorted = quickSort(myArray);
     console.log("quick sorted", sorted)
-    const otherArray = [-3, -1, 5, 100];
-    const otherSorted = quickSort(otherArray);
-	console.log("quick othersorted", otherSorted)
+    // const otherArray = [-3, -1, 5, 100];
+    // const otherSorted = quickSort(otherArray);
+	// console.log("quick othersorted", otherSorted)
     // //[-3, -1, 5, 100]
